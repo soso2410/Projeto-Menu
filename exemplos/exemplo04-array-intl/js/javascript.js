e56586848637b962f5bd5130379b60c9c4bc81b2
Glauco Todesco <glauco.todesco@abutua.com>
2022-05-23 16:54:25 -0300

    Refactoring

--- exemplos/exemplo04-array-intl/js/javascript.js
+++ exemplos/exemplo04-array-intl/js/javascript.js
@@ -1,19 +1,20 @@
-
-
 var prods = [
-    { id: 1, name: "Produto 1", price: 10.0 },
-    { id: 2, name: "Produto 2", price: 20.0 },
+    { id: 1, name: "Produto 1", price: 1000.0 },
+    { id: 2, name: "Produto 2", price: 2000.0 },
 ];
 
 function calc(){
-    saida.innerHTML = '';
+    
+    output = document.getElementById("output");
+    output.innerHTML = "";
     
     var formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
     });
 
-    for (let input of prods) {
-        saida.innerHTML += `Preço: ${formatter.format(prods[input.id-1].price)}</br>`
-    }
-}
+    
+    output.innerHTML += `Preço: ${formatter.format(prods[0].price)}</br>`
+    output.innerHTML += `Preço: ${formatter.format(prods[1].price)}</br>`
+    
+} 
